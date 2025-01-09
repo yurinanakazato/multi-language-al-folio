fff9a651bc6e467dbc4975b6ea28b996dba746f2
yurinanakazato <47862935+yurinanakazato@users.noreply.github.com>
2025-01-09 03:06:53 +0000

    Deploying to gh-pages from @ yurinanakazato/multi-language-al-folio@5cabeb7c47ad994085c78203b691a93edb323dcf 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "updated Jan 2025.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/multi-language-al-folio/cv/";
