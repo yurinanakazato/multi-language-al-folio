a4be7e0cf4ae0f639cc1fb705666ba980a034949
yurinanakazato <47862935+yurinanakazato@users.noreply.github.com>
2025-01-17 13:23:02 +0000

    Deploying to gh-pages from @ yurinanakazato/multi-language-al-folio@6047445cc8a27f36e5362705114e50ffe01674d7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,28 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/multi-language-al-folio/people/";
           },
-        },{id: "dropdown-publications",
-              title: "publications",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "";
-              },
-            },{id: "dropdown-projects",
-              title: "projects",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/multi-language-al-folio/blog/";
-              },
-            },{id: "post-a-post-with-image-galleries",
+        },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
       
